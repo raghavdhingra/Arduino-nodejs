eebaf37e471e2c3e05036f5b45ce42c8a93062ad
Raghav Dhingra <raghav.dhingra15@gmail.com>
2021-05-14 12:31:42 +0530

    esp servo control

--- esp-servo-control.js
+++ esp-servo-control.js
@@ -1,21 +1,31 @@
+'use strict';
 const { Servo } = require('johnny-five');
 const keypress = require('keypress');
+const { EtherPortClient } = require('etherport-client');
 const five = require('johnny-five');
 keypress(process.stdin);
 
-const board = new five.Board();
+const board = new five.Board({
+  port: new EtherPortClient({
+    host: '192.168.1.18',
+
+    port: 3030,
+  }),
+  repl: false,
+});
 
 board.on('ready', () => {
   console.log(
     'Use Up and Down arrows for CW and CCW respectively. Space to stop.'
   );
 
-  const servo = new Servo.Continuous(5);
+  const servo = new Servo({ pin: 14 });
 
   process.stdin.resume();
   process.stdin.setEncoding('utf8');
   process.stdin.setRawMode(true);
-
+  let pos = 0,
+    add = 50;
   process.stdin.on('keypress', (ch, key) => {
     if (!key) {
       return;
@@ -25,11 +35,13 @@ board.on('ready', () => {
       console.log('Quitting');
       process.exit();
     } else if (key.name === 'up') {
-      console.log('CW');
-      servo.cw();
+      if (pos <= 180) pos = pos + add;
+      console.log(pos);
+      servo.to(pos);
     } else if (key.name === 'down') {
-      console.log('CCW');
-      servo.ccw();
+      if (pos >= 0) pos = pos - add;
+      console.log(pos);
+      servo.to(pos);
     } else if (key.name === 'space') {
       console.log('Stopping');
       servo.stop();
