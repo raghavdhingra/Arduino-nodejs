3a8d1274902a566a75868e3f561e5780dd9dff17
Raghav Dhingra <raghav.dhingra15@gmail.com>
2021-05-14 11:25:18 +0530

    esp controller

---
+++ esp-led-control.js
@@ -0,0 +1,38 @@
+const { Servo } = require('johnny-five');
+const keypress = require('keypress');
+const five = require('johnny-five');
+keypress(process.stdin);
+
+const board = new five.Board();
+
+board.on('ready', () => {
+  console.log(
+    'Use Up and Down arrows for CW and CCW respectively. Space to stop.'
+  );
+
+  const servo = new Servo.Continuous(5);
+
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+  process.stdin.setRawMode(true);
+
+  process.stdin.on('keypress', (ch, key) => {
+    if (!key) {
+      return;
+    }
+
+    if (key.name === 'q') {
+      console.log('Quitting');
+      process.exit();
+    } else if (key.name === 'up') {
+      console.log('CW');
+      servo.cw();
+    } else if (key.name === 'down') {
+      console.log('CCW');
+      servo.ccw();
+    } else if (key.name === 'space') {
+      console.log('Stopping');
+      servo.stop();
+    }
+  });
+});
